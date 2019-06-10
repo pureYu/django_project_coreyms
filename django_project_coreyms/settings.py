fedab26a3c580c4077c99c34dd0a6f95624ce5f4
pure_yu <yuliya.s.belovol@gmail.com>
2019-06-10 14:24:06 +0300

    Added django-heroku

--- django_project_coreyms/settings.py
+++ django_project_coreyms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,3 +144,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'yuliya.b.dev@gmail.com'
 EMAIL_HOST_PASSWORD = 'yuliyaYu88'
 
+django_heroku.settings(locals())
